62483222d1c43e87b67534eb9e8f9a4f4babef53
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-06-02 18:44:00 +0530

    ⚡️ improve validation

--- web/src/components/validate.js
+++ web/src/components/validate.js
@@ -1,28 +1,37 @@
 const yup = require("yup");
 
 export const validate = async (name, data) => {
-  if (name === "Email") {
+  console.log(name);
+  if (name === "College Email") {
     try {
-      await emailObj.validate({ Email: data });
+      console.log(data);
+      await emailSchema.validate(data);
       return "";
     } catch (err) {
-      return err.message;
+      return "Email is malformed or missing!";
     }
   } else if (name === "Registration Number") {
     let regex = /^RA[0-9]{13}$/;
     if (!regex.test(data) || data === undefined) {
-      return "Registration number must start with RA and should be 13 characters";
+      return "Registration number must start with RA and should be 13 characters!";
     }
     return "";
   } else if (name === "Github Id") {
     if (data === undefined) {
-      return "Github Username is required";
+      return "Github Username is required!";
     }
     const res = await github(data);
     if (res) {
       return "";
     }
-    return "Github Id is not valid";
+    return "Github ID is not valid!";
+  } else if (name === "Project Link") {
+    try {
+      await linkSchema.validate(data);
+      return "";
+    } catch (err) {
+      return "Link is either malformed or not a GitHub Link!";
+    }
   } else {
     if (data.length === 0 || data === "") {
       return `${name} is required`;
@@ -37,6 +46,11 @@ const github = async (data) => {
   return res["message"] === undefined;
 };
 
-export const emailObj = yup.object().shape({
-  Email: yup.string().email().required(),
-});
+export const emailSchema = yup.string().trim().email().required();
+
+export const linkSchema = yup
+  .string()
+  .trim()
+  .url()
+  .matches(/^https:\/\/github.com\/[\w|\D]+\/[\w|\D]+$/)
+  .required();
