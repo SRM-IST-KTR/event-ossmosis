c2439b806108b50e2b80177d0a8fa88059e7751a
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-06-03 21:03:12 +0530

    🚑 fix debug var

--- server/server/settings.py
+++ server/server/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.token_hex()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
